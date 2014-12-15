fca0bdb48486c24f546154c5d87f475aa28f4c2f
root <root@fotorola.de>
2014-12-15 12:58:41 +0100

    dded some links to the index file.

--- index.php
+++ index.php
@@ -24,3 +24,18 @@
  * THE SOFTWARE.
  */
 
+require_once './code/App.php';
+
+?><!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo __('MyPhotoStorage') ?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+        <link rel="stylesheet" href="../static/css/default.css" />
+    </head>
+    <body>
+		<div><a href="account/login.php"><?php echo __('Login') ?></a></div>
+		<div><a href="account/register.php"><?php echo __('Register a new account') ?></a></div>
+    </body>
+</html>
